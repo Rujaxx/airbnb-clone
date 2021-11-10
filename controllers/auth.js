791046841db03517ca56750c2c3c8a305bdf2226
Rujaxx <shaikhrujan@gmail.com>
2021-11-10 15:22:22 +0530

    user register login

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,3 +11,30 @@ exports.register = async(req,res,next) => {
 
     res.status(200).json({ success : true, message : 'User registered successfully'})
 }
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = async(req,res,next) => {
+    const { email, password } = req.body
+
+    if(!email || !password){
+        res.status(400).json({ success: false, message : 'Please provide an email and password'})
+    }
+
+    //Check for user
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user){
+        res.status(404).json({ success: false, message : 'User not found'})
+    }
+
+    //Check if password matches
+    const isMatch = await user.matchPassword(password)
+
+    if(!isMatch){
+        res.status(401).json({ success : true, message : 'Invalid Credentials'})
+    }
+
+    res.status(200).json({ success : true, message : 'User logged in successfully'})
+}
--- server.js
+++ server.js
@@ -8,7 +8,16 @@ dotenv.config({ path : './config/config.env'})
 const connectDB = require('./config/db')
 connectDB()
 
+//Router files
+const auth = require('./routes/auth')
+
 const app = express()
+
+//Body Parser
+app.use(express.json())
+
+//Routes
+app.use('/api/v1/auth', auth)
 
 const PORT = process.env.PORT || 5000
 
